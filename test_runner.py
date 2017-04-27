d6cad98535ed14aa9c92a8b1340d9382ca3cbea0
Jesper Romers <j.c.romers@gmail.com>
2017-04-27 14:22:46 +0200

    use importlib instead of ugly hack

--- test_runner.py
+++ test_runner.py
@@ -1,11 +1,10 @@
 import inspect
+import importlib
 
 
 def run_tests():
     module_name = inspect.stack()[1].filename.split('.')[0]
-    exec('import {}'.format(module_name))
-    mod = eval(module_name)
-
+    mod = importlib.import_module(module_name)
     test_functions = (f for f in dir(mod) if f.startswith('test_'))
 
     print('Testing module {}'.format(module_name))
@@ -13,7 +12,7 @@ def run_tests():
     for test_function in test_functions:
         print('Testing function {}... '.format(test_function), end='')
         try:
-            exec('{}.{}()'.format(module_name, test_function))
+            exec('mod.{}()'.format(test_function))
             print('SUCCESS')
         except AssertionError:
             print('FAIL')
