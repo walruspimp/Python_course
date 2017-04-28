015ca769f233628cb0f6309e83af6486b26e21ec
KatjaT <katja.tummler@yahoo.de>
2017-04-28 10:26:34 +0200

    formatted exercise I

--- exercise_CalculatingFibonacci.py
+++ exercise_CalculatingFibonacci.py
@@ -1,43 +1,49 @@
-from test_runner import run_tests
-
-
-# Task 1
-# The Fibonacci series is a series of numbers in which each number is the sum of the two preceding numbers. 
-# The first 6 numbers of the Fibonacci series would then be: 1,1,2,3,5,8
-# You should design a program that calculates the first 1000 numbers of the Fibonacci series (having as the two starting numbers: 1,1)
-# and saves them in a list that at the end of the program will be printed.
-
-
-# ________________________________________
-# Your code here
+#!/usr/bin/env python
+"""
+In this excercise we will learn the general usage of functions and flow control in a python script. 
+We ask you to implemant a fucntion  to calculate the first n numbers of the Fibonacci series. 
+The Fibonacci series is a series of numbers in which each number is the sum of the two preceding numbers. 
+The first 6 numbers of the Fibonacci series would then be: 1,1,2,3,5,8
+You should design a program that calculates the first n numbers of the Fibonacci series (having as the two starting numbers: 1,1)
+and saves them in a list that at the end of the program will be printed.
+"""
 
 
 
-# define function
-def generate_fibonacci(nend)
+"""
+Put your code in this section of the script:
+"""
 
 
-# call function (with user input)
-list_fibonacci = generate_fibonacci(nend)
-print(list_fibonacci)
-    
+
+def generate_fibonacci(n):
+    """ Counts the number of words (separated by white spaces or newlines) in a text """
+    pass
 
 
-# ________________________________________
-# test part, do not change
+# call function & print output
+list_fibonacci = generate_fibonacci(n)
+
+
+
+
+
+"""
+From here on unit tests.
+Do not modify this code!
+"""
+
 def test_list_fibonacci():
-   list_fibonacci = generate_fibonacci(100)
-   assert list_fibonacci[-1] == 927372692193078999176
-   assert list_fibonacci[0] == 1
-   assert list_fibonacci[1] == 1
+    list_fibonacci = generate_fibonacci(100)
+    assert list_fibonacci[-1] == 927372692193078999176
+    assert list_fibonacci[0] == 1
+    assert list_fibonacci[1] == 1
 
-   list_fibonacci = generate_fibonacci(0)
-   assert list_fibonacci[-1] == 1
-   assert len(list_fibonacci) == 2
+    list_fibonacci = generate_fibonacci(0)
+    assert list_fibonacci[-1] == 1
+    assert len(list_fibonacci) == 2
 
 
 if __name__ == '__main__':
-   run_tests()
-
-
-
+    from test_runner import run_tests
+    run_tests()
