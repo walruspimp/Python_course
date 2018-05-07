59d4cb3b79d554e5961fd0b8aaa5c6f44a91a20d
derHahn <jens.hahn@hu-berlin.de>
2018-05-07 13:36:03 +0200

    changed assignments and solution for assignments lecture

--- assignment_1.py
+++ assignment_1.py
@@ -12,35 +12,35 @@ solution_student.append(True)  # Hier wird die erste Antwort an die Liste gehang
 # y = [1,2]
 # x == y ?? True or False ?
 
-solution_student.append()
+solution_student.append(None)
 
 # Assignment 02:
 # x = [1,2]
 # y = [1,2]
 # x is y ?? True or False ?
 
-solution_student.append()
+solution_student.append(None)
 
 # Assignment 03:
 # x = [1,2]
 # y = x
 # x == y ?? True or False ?
 
-solution_student.append()
+solution_student.append(None)
 
 # Assignment 04:
 # x = [1,2]
 # y = x
 # x is y ?? True or False ?
 
-solution_student.append()
+solution_student.append(None)
 
 # Assignment 05:
 # x = 5
 # y = 5
 # x is y ?? True or False ?
 
-solution_student.append()
+solution_student.append(None)
 
 # Assignment 06:
 # x = 5
@@ -48,23 +48,40 @@ solution_student.append()
 # x = 6
 # x == y ?? True or False ?
 
-solution_student.append()
+solution_student.append(None)
 
-# Assignment 06:
+# Assignment 07:
+# x = 5
+# y = x
+# x = 6
+# x is y ?? True or False ?
+
+solution_student.append(None)
+
+# Assignment 08:
 # x = 2
 # y = x
 # x = x + 1
 # x == y ?? True or False ?
 
-solution_student.append()
+solution_student.append(None)
 
-# Assignment 06:
+# Assignment 09:
 # x = [1,2]
 # y = x
 # x.append(3)
 # x is y ?? True or False ?
 
-solution_student.append()
+solution_student.append(None)
+
+# Assignment 10:
+# x = [1,2]
+# y = x
+# x.append(3)
+# x == y ?? True or False ?
+
+solution_student.append(None)
+
 
 if __name__ == '__main__':
     import _pickle
@@ -73,4 +90,10 @@ if __name__ == '__main__':
     if solution_tutors == solution_student:
         print('Solved!')
     else:
-        print('Try Again!')
+        false_answers = []
+        for index, answer in enumerate(solution_student):
+            if answer != solution_tutors[index]:
+                false_answers.append(str(index))
+            else:
+                pass
+        print('Try Again! Answer(s) for the assignment(s) {} are wrong'.format(', '.join(false_answers)))
