cd7d0a0997e42c88bf96b5b8d5f14d32bfcb91c3
derHahn <jens.hahn@hu-berlin.de>
2018-05-07 13:59:35 +0200

    made homework more verbose

--- homework_1.py
+++ homework_1.py
@@ -17,8 +17,13 @@
 # 8. push this file in your repository
 
 if __name__ == '__main__':
-    assert type(z) == list, 'z is not a list'
-    assert len(z) <= 2, 'list length is too long'
-    assert len(z) >= 2, 'list length is too short'
-    assert type(z[0]) == int, 'first element in the list is no integer'
-    assert type(z[1]) == int, 'second element in the list is no integer'
+    assert type(x) is int, '1. x is not an integer'
+    assert y is x, '2. y is not assigned to the content of x'
+    assert type(x) is int, '3. x is not an integer anymore'
+    assert type(y) is int, '4. y is not an integer anymore'
+    assert type(z) == list, '5. z is not a list'
+    assert len(z) <= 2, '6. list length is too long'
+    assert len(z) >= 2, '6. list length is too short'
+    assert type(z[0]) == int, '6. first element in the list is no integer'
+    assert type(z[1]) == int, '6. second element in the list is no integer'
+    print('Congratulations, homework successfully solved!')
