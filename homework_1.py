aab1cd457b58c52ffb71185e006853637a63be3f
derHahn <jens.hahn@hu-berlin.de>
2018-05-07 13:53:10 +0200

    added homework 1 to repo

---
+++ homework_1.py
@@ -0,0 +1,24 @@
+# fill the python code
+
+# 1. assign an integer to the variable x
+
+# 2. assign the content of x to the variable y
+
+# 3. increase the content of x by 13
+
+# 4. decrease the content of y by 3
+
+# 5. assign an empty list to the variable z
+
+# 6. put x and y in this list
+
+# 7. run this file in Python3 and make sure no error occurs
+
+# 8. push this file in your repository
+
+if __name__ == '__main__':
+    assert type(z) == list, 'z is not a list'
+    assert len(z) <= 2, 'list length is too long'
+    assert len(z) >= 2, 'list length is too short'
+    assert type(z[0]) == int, 'first element in the list is no integer'
+    assert type(z[1]) == int, 'second element in the list is no integer'
