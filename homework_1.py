38e282ead1a31b7f00838ded5f86be6e19ee30ef
walruspimp <camillodavid@aol.com>
2018-05-14 11:13:21 +0200

    homework_1.py

--- homework_1.py
+++ homework_1.py
@@ -1,19 +1,19 @@
 # fill the python code
 
 # 1. assign an integer to the variable x
-
+x = 2
 # 2. assign the content of x to the variable y
-
+y = x
 # 3. increase the content of x by 13
-
+x = x + 13
 # 4. decrease the content of y by 3
-
+y = y + 3
 # 5. assign an empty list to the variable z
-
+z = []
 # 6. put x and y in this list
-
+z.append(x)
 # 7. run this file in Python3 and make sure no error occurs
-
+z.append(y)
 # 8. push this file in your repository
 
 if __name__ == '__main__':
