0a266db6c3ee02f83f83d8be76ddcacd373582c2
derHahn <jens.hahn@hu-berlin.de>
2018-05-15 15:25:15 +0200

    minor changes in homework

--- homework_2.py
+++ homework_2.py
@@ -3,12 +3,13 @@
 # These variables contain the strings `rock`, `paper', or 'scissors'
 # Implement the rules as if/else/elif statements after line 10 with an indentation of 4 spaces, see example (line 10+11)
 # Return a string saying 'tied game', 'you win', or 'computer wins' after every statement
-# If there is no error when you run the script, you succeded
+# If there is no error when you run the script, you succeeded
 # Push the solution in your repo
 
+
 def rock_paper_scissors(user_input, computer_input):
-	if user_input is 'rock' and computer_input is 'paper':
-		return 'computer wins'
+    if user_input is 'rock' and computer_input is 'paper':
+        return 'computer wins'
 
 
 
@@ -17,8 +18,8 @@ def rock_paper_scissors(user_input, computer_input):
 
 
 if __name__ == "__main__":
-	possibilities = ['rock', 'paper', 'scissors']
-	for user_choice in possibilities:
-		for computer_choice in possibilities:
-			if user_choice == computer_choice:
-				assert rock_paper_scissors(user_choice, computer_choice) is 'tied game'
+    possibilities = ['rock', 'paper', 'scissors']
+    for user_choice in possibilities:
+        for computer_choice in possibilities:
+            if user_choice == computer_choice:
+                assert rock_paper_scissors(user_choice, computer_choice) is 'tied game'
