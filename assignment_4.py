9a20820808413a136417b0824dd8f411e6a002e2
Jens Hahn <jens.hahn@hu-berlin.de>
2018-06-04 14:58:34 +0200

    added assingment and slides

---
+++ assignment_4.py
@@ -0,0 +1,49 @@
+import time
+
+class RockPaperScissors:
+    """
+    Implementation of the game Rock, Paper, Scissors
+    """
+    def __init__(self):
+        print('meep')
+        pass
+
+    def computer_choice(self):
+        """
+        The computer chooses a value
+        """
+        pass
+
+    def human_choice(self):
+        """
+        The human player chooses a value
+        """
+        pass
+
+    def evaluate_game(self):
+        """
+        Evaluate the values of the two players
+        """
+        pass
+
+    def print_result(self):
+        """
+        Visualise game result
+        """
+        pass
+
+    def count_points(self):
+        """
+        Count the points of all rounds
+        """
+        pass
+
+    def new_game_question(self):
+        """
+        Ask human player for new game
+        """
+        pass
+
+
+if __name__ == "__main__":
+    RockPaperScissors()
